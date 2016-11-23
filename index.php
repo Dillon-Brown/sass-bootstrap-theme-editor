350a3b6890c16714711fc4e7b8c1d85637f27e8b
Daniel Samson <daniel.samson@salesagility.com>
2016-11-23 12:14:57 +0000

    Adding jquery ui

--- index.php
+++ index.php
@@ -5,18 +5,12 @@
     <title>SASS In The Browser</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/farbtastic.css">
+    <link rel="stylesheet" href="css/jquery-ui.min.css">
 <body>
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Project name</a>
-        </div>
+
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Home</a></li>
@@ -33,17 +27,75 @@
         <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
     </div>
 
+    <form action="" style="width: 400px;">
+        <div class="form-item"><label for="color">Navbar Color:</label><input type="text" id="color" name="color" value="#534D64" /></div><div id="picker"></div>
+    </form>
+    <label for="slider">Navbar Height:</label>
+    <div id="slider"></div>
 </div>
+
+<style id="scss_result"></style>
 
 <script src="js/jquery-3.1.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/farbtastic.js"></script>
 <script src="dist/sass.js"></script>
 <script>
-    var scss = '$someVar: 123px; .some-selector { width: $someVar; }';
     var sass = new Sass();
-    sass.compile(scss, function(result) {
-        console.log(result);
+    var color_scss = '$navbar-bg-color: #534D64; ';
+    var val_scss = '$navbar-height: 20px; ';
+    var style_scss = '.navbar-inverse { background-color: $navbar-bg-color; min-height: $navbar-height; }' +
+        '.navbar-nav>li>a { padding-top: ($navbar-height / 2); padding-bottom: ($navbar-height / 2);  } ' +
+        '.navbar-brand { padding-top: ($navbar-height / 2); padding-bottom: ($navbar-height / 2); } ' +
+        '.main-container { margin-top: $navbar-height + 30px; } ';
+
+    function ColorCallback(color) {
+        color_scss = '$navbar-bg-color: ' + color + ';';
+        $('#color').val(color);
+            sass.compile(val_scss + color_scss + style_scss, function(result) {
+                $('#scss_result').html(result.text);
+                UpdateDisplay();
+            });
+    }
+
+    function UpdateDisplay() {
+
+    }
+
+
+        $('#color').farbtastic('#picker');
+        $.farbtastic('#picker').linkTo(ColorCallback);
+        $.farbtastic('#picker').setColor($('#color').val());
+        $('#color').change(ColorCallback);
+
+
+        $( "#slider" ).slider({
+            value: 20,
+            min: 20,
+            max: 200,
+            step: 8,
+            slide: function( event, ui ) {
+                console.log( ui.value);
+                val_scss = '$navbar-height:'+ ui.value +'px; ';
+                sass.compile(val_scss + color_scss + style_scss, function(result) {
+//                    console.log(result)
+                    $('#scss_result').html(result.text);
+                    UpdateDisplay();
+                });
+            }
+        });
+
+
+    // first compile
+    sass.compile(val_scss + color_scss + style_scss, function(result) {
+        console.log(result)
+        $('#scss_result').html(result.text);
+        UpdateDisplay();
     });
+
+
+
 </script>
 </body>
 </html>
