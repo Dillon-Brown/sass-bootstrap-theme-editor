b901359e7ac68e0f7f619de060fda0983d6064f1
Daniel Samson <daniel.samson@salesagiliy.com>
2017-03-31 09:15:42 +0100

    moving bootstrap to bower removing composer.json

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
 <head>
     <title>SASS In The Browser</title>
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap-grid.min.css">
-    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-reboot.min.css">
     <link rel="stylesheet" href="src/css/farbtastic.css">
     <link rel="stylesheet" href="src/css/jquery-ui.min.css">
     <script src="bower_components/jquery/dist/jquery.min.js"></script>
     <script src="bower_components/jquery-ui/jquery-ui.js"></script>
     <script src="bower_components/tether/dist/js/tether.min.js"></script>
-    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="src/js/farbtastic.js"></script>
     <script src="src/dist/sass.js"></script>
     <script src="src/js/sass-theme-editor.js"></script>
